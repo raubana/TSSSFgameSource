5a75887ebcc4b8c75f2712cd120d7dd242c3e67a
raubana <dylanjraub@yahoo.com>
2014-09-19 08:55:26 -0400

    Changed Server and Client to use 'close' followed by 'shutdown', since close seems to close a connection clearly, while shutdown guarantees a closed connection.

--- libs/netcom.py
+++ libs/netcom.py
@@ -150,6 +150,8 @@ class Server(object):
 	def disconnect(self, address):
 		print "-Killing connection with",address,"..."
 		if address in self.clients:
+			try: self.clients[address].close()
+			except: pass
 			try: self.clients[address].shutdown(socket.SHUT_RDWR)
 			except: pass
 			try: del self.clients[address]
@@ -250,6 +252,8 @@ class Client(object):
 		#self.listen_thread.exit()
 		#self.serversocket.shutdown(socket.SHUT_RDWR)
 		self.connected = False
+		try: self.serversocket.close()
+		except: pass
 		try: self.serversocket.shutdown(socket.SHUT_RDWR)
 		except: pass
 		try: del self.serversocket
